a108687041904475993346a8c978439a138ed0d7
D310820 <tecnocel30210@gmail.com>
2026-05-29 17:44:19 -0600

    Initial Commit

---
+++ TaskHup1/TaskHup1App.swift
@@ -0,0 +1,17 @@
+//
+//  TaskHup1App.swift
+//  TaskHup1
+//
+//  Created by Jose Daniel Espinoza Gomez on 29/05/26.
+//
+
+import SwiftUI
+
+@main
+struct TaskHup1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
